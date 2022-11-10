90859167a71036deb6f5710c69bb5905fae51dde
Akshay  Kumar <>
2022-11-10 16:56:39 +0000

    problem10

--- set01/problem10.c
+++ set01/problem10.c
@@ -1,20 +1,43 @@
- #include<stdio.h>
+#include<stdio.h>
+#include<string.h>
+
 void input_two_strings(char *string1, char *string2);
 int stringcompare(char *string1, char *string2);
-void output(char *string1, char *string2);
-int main()
-{
-  char str1,str2;
-  input_two_strings(&str1,&str2);
-  output(&str1,&str2);
-  
+void output(char *string1, char *string2, int result);
+
+int main(){
+  char str1[100],str2[100];
+  int res;
+  input_two_strings(str1,str2);
+  res=stringcompare(str1,str2);
+  output(str1,str2,res);
 }
-void input_two_strings(char[100] *string1, char[100] *string2)
+
+void input_two_strings(char *string1, char *string2)
 {
-  scanf("%s",&string1);
-  scanf("%s",&string2);
+  scanf("%s %s",&string1,&string2);
 }
-void output(char *string1, char *string2)
+
+int stringcompare(char *string1, char *string2)
 {
-  printf("%s\n%s\n",string1,string2);
+  int result;
+  result=strcmp(string1,string2);
+  return result;
 }
+
+void output(char *string1, char *string2, int result)
+{
+  if(result==0)
+  {
+    printf("strings are equal");
+  }
+  else if(result>1)
+  {
+    printf("string one is great");
+  }
+  else
+  {
+    printf("string 2 is great");
+  }
+}
+
